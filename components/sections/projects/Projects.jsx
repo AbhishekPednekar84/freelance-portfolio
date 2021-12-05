6db875522f2cb91dec0d178a22b71639689c0b50
Abhishek Pednekar <abhi_ap@hotmail.com>
2021-12-05 17:47:00 +0530

    Fixed the projects sections for removed testimonials

--- components/sections/projects/Projects.jsx
+++ components/sections/projects/Projects.jsx
@@ -5,7 +5,7 @@ import ProjectGrid from "./ProjectGrid";
 
 const Projects = () => {
   return (
-    <div className="md:pb-24 mx-5 md:mx-24 -my-10">
+    <div className="pb-12 md:pb-24 mx-5 md:mx-24 -mt-10">
       <h2 className="pb-12 lg:pb-20 text-center">
         <span className="pb-2 border-b-4 border-blue-500">Pa</span>st Projects
       </h2>
