b6c073a01d5a50f6ea458f3924c041970f90c612
Abhishek Pednekar <abhi_ap@hotmail.com>
2021-12-07 19:57:27 +0530

    Moved clarity script to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,19 +10,23 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+             })(window, document, "clarity", "script", "9n0p3k99s5");
+            `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
-          {/* <div
-            id="wcb"
-            class="carbonbadge"
-            style={{
-              backgroundColor: "#f3ecd899",
-              paddingTop: "5px",
-              paddingBottom: "20px",
-            }}
-          ></div> */}
         </body>
       </Html>
     );
